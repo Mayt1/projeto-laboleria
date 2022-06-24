0055f41c48ceac3dd00d07192556fbd2867430a8
VictorUlian <yctor.ulian@gmail.com>
2022-06-24 18:16:41 -0300

    Initial files

---
+++ database/tables.sql
@@ -0,0 +1,23 @@
+CREATE TABLE cakes(
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    price BIGINT NOT NULL, ---TALVES DA ERRO AQUI tem q ser float
+    image TEXT NOT NULL,
+    description TEXT NOT NULL
+);
+
+CREATE TABLE clients (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    address TEXT NOT NULL,
+    phone TEXT NOT NULL
+);
+
+CREATE TABLE orders(
+    id SERIAL PRIMARY KEY,
+    totalPrice BIGINT NOT NULL, ---TALVES DA ERRO AQUI tem q ser float
+    quantity BIGINT NOT NULL,
+    "clientId" INTEGER REFERENCES clients(id),
+    "cakeId" INTEGER REFERENCES cakes(id),
+    "createdAt" TIMESTAMP DEFAULT NOW()
+);
