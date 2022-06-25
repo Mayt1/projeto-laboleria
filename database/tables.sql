74b6f6e2b945c9d41785ae2ef9469c23e38cb61d
VictorUlian <yctor.ulian@gmail.com>
2022-06-24 21:45:10 -0300

    fix: Now endpoint /orders can display all data from all orders

--- database/tables.sql
+++ database/tables.sql
@@ -1,7 +1,7 @@
 CREATE TABLE cakes(
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
-    price BIGINT NOT NULL, ---TALVES DA ERRO AQUI tem q ser float
+    price decimal NOT NULL, 
     image TEXT NOT NULL,
     description TEXT
 );
@@ -15,7 +15,7 @@ CREATE TABLE clients (
 
 CREATE TABLE orders(
     id SERIAL PRIMARY KEY,
-    totalPrice BIGINT NOT NULL, ---TALVES DA ERRO AQUI tem q ser float
+    totalPrice decimal NOT NULL,
     quantity BIGINT NOT NULL,
     "clientId" INTEGER REFERENCES clients(id),
     "cakeId" INTEGER REFERENCES cakes(id),
