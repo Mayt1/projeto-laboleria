8194c79a9a05db12a07c8842a02d9210489dd517
VictorUlian <yctor.ulian@gmail.com>
2022-06-24 21:27:12 -0300

    feat: Adding endpoint /clients/:id/orders that get all orders from a client

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -75,36 +75,40 @@ export async function getOrders(req, res) {
 export async function getAllOrders(req, res) {
     try{
         //vem os dados da ordem
-        const ordersData = await db.query('SELECT * FROM orders');
+        const ordersData = await db.query('select * from orders as ors JOIN cakes as cak ON cak.id = ors."cakeid" JOIN clients as cls ON cls.id = ors."clientid"');
         if(ordersData.rowCount == 0){
             return res.sendStatus(404);
         }
 
-        console.log(ordersData.rows)//aqui vem todos os ids
-        const [orders] = ordersData.rows;
-
-        console.log(ordersData.rows.cakeid)
-        //vem os dados do bolo
-        const cakeData = await db.query('SELECT * FROM cakes WHERE id = $1', [orders.cakeid]);
-        if(cakeData.rowCount == 0){
-            return res.sendStatus(404);
-        }
-
-        //vem os dados do cliente
-        const clientData = await db.query('SELECT * FROM clients WHERE id = $1', [orders.clientid]);
-        if(clientData.rowCount == 0){
-            return res.sendStatus(404);
-        }
-        console.log(orders.id)
-        //montar a resposta
         res.send({
-            idOrder: orders.id,
-            client: clientData.rows,
-            cake: cakeData.rows,
-            createdAt: orders.createdAt,
-            totalPrice :  orders.totalprice,
-            quantity:  orders.quantity,
-        }).status(200)
+            Orders: ordersData.rows.id
+        })
+
+        // console.log(ordersData.rows)//aqui vem todos os ids
+        // const [orders] = ordersData.rows;
+
+        // console.log(ordersData.rows.cakeid)
+        // //vem os dados do bolo
+        // const cakeData = await db.query('SELECT * FROM cakes WHERE id = $1', [orders.cakeid]);
+        // if(cakeData.rowCount == 0){
+        //     return res.sendStatus(404);
+        // }
+
+        // //vem os dados do cliente
+        // const clientData = await db.query('SELECT * FROM clients WHERE id = $1', [orders.clientid]);
+        // if(clientData.rowCount == 0){
+        //     return res.sendStatus(404);
+        // }
+        // console.log(orders.id)
+        //montar a resposta
+        // res.send({
+        //     idOrder: orders.id,
+        //     client: clientData.rows,
+        //     cake: cakeData.rows,
+        //     createdAt: orders.createdAt,
+        //     totalPrice :  orders.totalprice,
+        //     quantity:  orders.quantity,
+        // }).status(200)
 
     } catch (error) {
         console.error(error);
@@ -112,3 +116,31 @@ export async function getAllOrders(req, res) {
     }
 }
 
+export async function getClientOrders(req, res) {
+    const { id } = req.params;
+    try{
+        const orderData = await db.query(`
+        SELECT ors.id, ors.quantity, ors.totalprice, ors."createdAt", cak.name 
+        FROM orders as ors 
+        JOIN clients as cls ON ors."clientid" =  $1
+        JOIN cakes as cak ON cak.id = ors."cakeid" 
+        GROUP BY ors.id, cak.id 
+        ORDER BY ors.id, cak.id ASC`, [id]);                                  
+        if(orderData.rowCount == 0){
+            return res.sendStatus(404);
+        }
+        res.send({
+            Orders: orderData.rows
+        }).status(200)
+        // res.send({
+        //     orderId: orderData.rows.id,
+        //     quantity: orderData.rows.quantity,
+        //     createdAt: orderData.rows.createdAt,
+        //     cakeName: orderData.rows.name
+        // }).status(200)
+
+    } catch (error) {
+        console.error(error);
+        return res.sendStatus(500);
+    }
+}
--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createOrder, getAllOrders, getOrders } from "../controllers/orderController.js";
+import { createOrder, getAllOrders, getClientOrders, getOrders } from "../controllers/orderController.js";
 
 const orderRouter = Router();
 orderRouter.post("/order", createOrder);
 orderRouter.get("/orders/:id", getOrders)
 orderRouter.get("/orders", getAllOrders)
+orderRouter.get("/client/:id/orders", getClientOrders)
 
 export default orderRouter;
